ffd0b46ee7d868332b35baa6d854116014b1189c
FranklinMiranda <fdm61344@gmail.com>
2022-12-14 17:22:33 -0500

    CM

---
+++ react-travel-blog/src/Components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class App extends React.Component {
+  render(){
+    return (
+      <h1>Hello React, this is the App Component</h1>
+    )
+  }
+}
+
+export default App
