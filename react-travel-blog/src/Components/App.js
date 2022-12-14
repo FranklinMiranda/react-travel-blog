b604d435804be00692db70d7ed1f654312a3eb0d
FranklinMiranda <fdm61344@gmail.com>
2022-12-14 18:48:03 -0500

    CM

--- react-travel-blog/src/Components/App.js
+++ react-travel-blog/src/Components/App.js
@@ -4,7 +4,16 @@ import Article from './Article';
 import ArticleData from '../Data/ArticleData';
 
 const ArticleEl = ArticleData.map((el) => {
-  return <Article img={el.img} title={el.title} date={el.date} location={el.location} hyperlink={el.hyperlink} description={el.description} />;
+  return (
+    <Article
+      img={el.img}
+      title={el.title}
+      date={el.date}
+      location={el.location}
+      hyperlink={el.hyperlink}
+      description={el.description}
+    />
+  );
 });
 
 class App extends React.Component {
