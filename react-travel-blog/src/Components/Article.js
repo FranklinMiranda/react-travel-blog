f91660fd4214b40f3c80531783ed5cb31e3e4377
FranklinMiranda <fdm61344@gmail.com>
2022-12-14 17:57:14 -0500

    CM

--- react-travel-blog/src/Components/Article.js
+++ react-travel-blog/src/Components/Article.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import Img from '../Images/logo192.png';
 
 class Article extends React.Component {
   render(props) {
-    return <div className="Article">
-        <h1>{this.props.title}</h1>
-    </div>;
+    return (
+      <div className="Article">
+        <div className="Article-Image">
+          {' '}
+          <img src={Img} alt="React Logo"></img>
+        </div>
+        <div className="Article-Body">
+          <div className="Article-Title">
+            <h1>{this.props.title}!</h1>
+          </div>
+          <div className="Article-SubTitle">
+            <h2>{this.props.date}!</h2>
+            <h2>{this.props.location}!</h2>
+          </div>
+          <div className="Article-Description">
+            <p>{this.props.description}!</p>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-export default Article
+export default Article;
--- react-travel-blog/src/Components/Nav.js
+++ react-travel-blog/src/Components/Nav.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Img from '../Images/logo192.png'
 
 class Nav extends React.Component {
   render() {
     return (
       <div className="Nav">
         <h1>Franklin Miranda's Travel Blog</h1>
-        <img src={Img} alt="React Logo"></img>
       </div>
     );
   }
