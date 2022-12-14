716d6d601b510d2967b69120d5acf5bf2aae7dd0
FranklinMiranda <fdm61344@gmail.com>
2022-12-14 17:40:42 -0500

    CM

--- react-travel-blog/src/Components/Article.js
+++ react-travel-blog/src/Components/Article.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Article extends React.Component {
   render(props) {
     return <div className="Article">
-        
+        <h1>{this.props.title}</h1>
     </div>;
   }
 }
---
+++ react-travel-blog/src/Data/ArticleData.js
@@ -0,0 +1,10 @@
+// This file contains all the data that will populate the Article Components props parameter
+
+const ArticleData = [
+  {
+    title: 'Rehoboth Beach',
+    date: 'May, 2022',
+    location: 'Rehoboth Beach, Delaware',
+    description: 'Travel to Rehoboth Beach for summer vacation with the Family',
+  },
+];
